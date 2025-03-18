83b6f8641f406d6cc29263ebe96c28df559926f2
Zaccaria Cesaro <z.cesaro@donboscosandona.it>
2025-03-18 09:13:13 +0100

    Modifica il gestore di eventi per il pulsante UDP per commentare un ciclo di invio

--- App.tsx
+++ App.tsx
@@ -7,7 +7,7 @@ export default function Index() {
   
   return (
     <View style={{padding: 50}}>
-      <TouchableOpacity onPress={() => sendThreeBytes(4, 1, 2)}>
+      <TouchableOpacity onPress={() => {/* for(let i = 1; i <= 6; i++ ) */sendThreeBytes(4, 1, 2)}}>
         <Text>UDP</Text>
       </TouchableOpacity>
     </View>
