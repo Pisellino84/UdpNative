d05a4e3d1c8851252a0fb64495b39ae1c6a01bf9
Zaccaria Cesaro <z.cesaro@donboscosandona.it>
2025-03-25 16:01:10 +0100

    -------

--- lib/udpProtocol.tsx
+++ lib/udpProtocol.tsx
@@ -2,9 +2,14 @@ import dgram from 'react-native-udp';
 import {Buffer} from 'buffer';
 import {Alert} from 'react-native';
 
+// POSSIBILI PROBLEMI PER CUI RIESCO A INVIARE I DATI MA NON RIESCO A RICEVERLI:
+//    1. Bug dell'emulatore (se possibile bisognerebbe provare su un dispositivo fisico);
+//    2. Problemi di rete (router, firewall(provato a disattivare insieme all'antivirus), antivirus, etc.);
+//    3. Problemi di configurazione dell' UDP (porta, IP, etc.);
+
 const PORT = 53280;
-const HOST = '192.168.30.211'; // 192.168.30.211 (al momento non usare nel socket.bind())
-const socket = dgram.createSocket({type: 'udp4', debug: true}); // Crea il socket una sola volta
+const HOST = '192.168.30.211'; 
+const socket = dgram.createSocket({type: 'udp4'}); // Crea il socket una sola volta
 
 socket.bind(PORT, (err: any) => {
   // Associa il socket all'avvio
@@ -12,15 +17,40 @@ socket.bind(PORT, (err: any) => {
     console.error("Errore durante l'associazione del socket:", err);
   } else {
     console.log(
-      `Socket UDP in ascolto - Port: ${PORT} - IP: ${HOST}`,
+      `Socket UDP in ascolto - Port: ${PORT}`,
     );
   }
 });
 
+socket.on('error', (err: any) => {
+  console.error("Errore del socket:", err);
+  Alert.alert('Errore del socket');
+});
+
 
 socket.on('message', function (msg, rinfo) {
-    console.log('Message received', msg.toString("hex"), rinfo);
-  });
+  console.log('Message received (hex)', msg.toString("hex"), rinfo);
+  console.log('Message received (string)', msg.toString(), rinfo);
+  console.log('Message received (buffer)', msg, rinfo);
+});
+
+const timeoutDuration = 5000; // 5000 millisecondi = 5 secondi
+let timeout: NodeJS.Timeout | null = null;
+
+function resetTimeout() {
+  if (timeout) {
+    clearTimeout(timeout);
+  }
+  timeout = setTimeout(() => {
+    console.log("Socket timeout!");
+    // Handle timeout logic here
+  }, timeoutDuration);
+}
+
+// Initialize timeout when the socket is bound
+resetTimeout();
+
+
   
 
 export async function sendThreeBytes(
